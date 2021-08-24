a9b560abe3bd532a2591d52da5484bf99d1346fa
fansa <1950577233@qq.com>
2021-08-24 21:46:00 +0800

    第一次添加

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
+import router from './router/index'
+import store from './store/index'
+import '../public/iconfont/iconfont.css'
 Vue.config.productionTip = false
+
+//axios依赖的配置
+import  axios  from "@/utils/request";
+Vue.prototype.$axios = axios;
+
+//配置elmentUI组件库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+//配置vantUI组件库
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
 
 new Vue({
   router,
